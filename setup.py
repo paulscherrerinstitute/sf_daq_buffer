5ac7ce00e13d2cd047841ec767374912faf3f5da
Simon Ebner <simon.ebner@psi.ch>
2020-04-30 09:25:27 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cadump",
-    version="0.0.8",
+    version="0.0.9",
     author="Paul Scherrer Institute",
     author_email="daq@psi.ch",
     description="Interface to dump data from archiver/databuffer",
