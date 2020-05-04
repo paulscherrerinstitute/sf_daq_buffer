7bcfc571a3d5e72047d54a698b0d47e8306b199c
Andrej Babic <andrej.babic@psi.ch>
2020-05-04 18:09:58 +0200

    Change start to 4 per core

--- scripts/JF07-buffer-worker.sh
+++ scripts/JF07-buffer-worker.sh
@@ -9,9 +9,12 @@ fi
 M=$1
 
 #8 udp2buffer workers per core
-#coreAssociatedBuffer=(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8)
+#coreAssociatedBuffer=(1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4)
 #4 udp2buffer workers per core
-coreAssociatedBuffer=(1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16)
+coreAssociatedBuffer=(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8)
+#2 udp2buffer workers per core
+#coreAssociatedBuffer=(1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16)
+
 initialUDPport=50100
 # strange that it doesn't work to add 08 or 09 (value too great for base (error token is "09"))
 port=$((${initialUDPport}+10#${M}))
