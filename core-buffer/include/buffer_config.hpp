b436ba12da629efbed53611a7bd155eceffe1105
Andrej Babic <andrej.babic@psi.ch>
2020-05-04 09:53:27 +0200

    Add parameters for new sf_buffer implementation

--- core-buffer/include/buffer_config.hpp
+++ core-buffer/include/buffer_config.hpp
@@ -26,7 +26,10 @@ namespace core_buffer {
     const size_t REPLAY_READ_BLOCK_SIZE = 100;
 
     // Size of sf_buffer RB in elements.
-    const size_t BUFFER_RB_SIZE = 1000;
+    const size_t BUFFER_INTERNAL_QUEUE_SIZE = 1000;
+
+    // Time to sleep before retrying to read the queue.
+    const size_t BUFFER_QUEUE_RETRY_MS = 10;
 
     // Microseconds timeout for UDP recv.
     const int BUFFER_UDP_US_TIMEOUT = 10 * 1000;
