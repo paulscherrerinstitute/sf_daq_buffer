8148802325c07caa9e7502f1d3a6e2cb639b67f8
Andrej Babic <andrej.babic@psi.ch>
2020-05-04 09:16:25 +0200

    Add new parameter for buffer

--- core-buffer/include/buffer_config.hpp
+++ core-buffer/include/buffer_config.hpp
@@ -31,6 +31,9 @@ namespace core_buffer {
     // Microseconds timeout for UDP recv.
     const int BUFFER_UDP_US_TIMEOUT = 10 * 1000;
 
+    // Output queue length for buffer live stream.
+    const int BUFFER_LIVE_SEND_HWM = 10;
+
     // ZMQ threads for receiving data from sf_replay.
     const int WRITER_ZMQ_IO_THREADS = 2;
 
