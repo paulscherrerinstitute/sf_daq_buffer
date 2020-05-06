adcd3c598812cfcad6229e0ab40b67f21a0f9f2e
Andrej Babic <andrej.babic@psi.ch>
2020-05-06 16:59:24 +0200

    Add 1 module 1 core policy

--- scripts/JF07-buffer-worker.sh
+++ scripts/JF07-buffer-worker.sh
@@ -13,7 +13,9 @@ M=$1
 #4 udp2buffer workers per core
 #coreAssociatedBuffer=(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8)
 #2 udp2buffer workers per core
-coreAssociatedBuffer=(1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16)
+#coreAssociatedBuffer=(1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16)
+# 1 udp2buffer worker per core
+coreAssociatedBuffer=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
 
 initialUDPport=50100
 # strange that it doesn't work to add 08 or 09 (value too great for base (error token is "09"))
