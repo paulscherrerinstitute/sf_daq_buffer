fd33f72a091473f1d05137daf06dede22c16a1dd
Andrej Babic <andrej.babic@psi.ch>
2020-05-08 11:39:36 +0200

    Invert bind and connect among replay and writer

--- sf-buffer/src/sf_writer.cpp
+++ sf-buffer/src/sf_writer.cpp
@@ -41,7 +41,7 @@ void receive_replay(
             ipc_addr << ipc_prefix << i;
             const auto ipc = ipc_addr.str();
 
-            if (zmq_bind(sockets[i], ipc.c_str()) != 0) {
+            if (zmq_connect(sockets[i], ipc.c_str()) != 0) {
                 throw runtime_error(strerror(errno));
             }
         }
