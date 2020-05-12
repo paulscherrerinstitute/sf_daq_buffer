14a50fb08ece60f767e34bf7777fca5374169e60
Andrej Babic <andrej.babic@psi.ch>
2020-05-12 09:54:43 +0200

    Implement recvmmsg in BufferUdpReceiver

--- core-buffer/include/BufferUdpReceiver.hpp
+++ core-buffer/include/BufferUdpReceiver.hpp
@@ -1,19 +1,30 @@
 #ifndef SF_DAQ_BUFFER_BUFFERUDPRECEIVER_HPP
 #define SF_DAQ_BUFFER_BUFFERUDPRECEIVER_HPP
 
+#include <netinet/in.h>
 #include "UdpReceiver.hpp"
 #include "jungfrau.hpp"
+#include "buffer_config.hpp"
 
 class BufferUdpReceiver {
     const int source_id_;
 
     UdpReceiver udp_receiver_;
-    jungfrau_packet packet_buffer_ = {};
-    bool packet_buffer_loaded_ = false;
 
-    inline void init_frame(ModuleFrame& frame_metadata);
+    jungfrau_packet packet_buffer_[core_buffer::BUFFER_UDP_N_RECV_MSG];
+    iovec recv_buff_ptr_[core_buffer::BUFFER_UDP_N_RECV_MSG];
+    mmsghdr msgs_[core_buffer::BUFFER_UDP_N_RECV_MSG];
+    sockaddr_in sock_from_[core_buffer::BUFFER_UDP_N_RECV_MSG];
+
+    bool packet_buffer_loaded_ = false;
+    int packet_buffer_n_packets_ = 0;
+    int packet_buffer_offset_ = 0;
+
+    inline void init_frame(ModuleFrame& frame_metadata, const int i_packet);
     inline void copy_packet_to_buffers(
-            ModuleFrame& metadata, char* frame_buffer);
+            ModuleFrame& metadata, char* frame_buffer, const int i_packet);
+    inline uint64_t process_packets(
+            const int n_packets, ModuleFrame& metadata, char* frame_buffer);
 
 public:
     BufferUdpReceiver(const uint16_t port, const int source_id);
