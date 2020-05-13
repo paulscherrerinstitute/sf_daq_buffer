32b66ed5f7a50f3444e02040dd111c9526e66e1a
Andrej Babic <andrej.babic@psi.ch>
2020-05-13 13:08:05 +0200

    Moved to 4 receivers per core

--- scripts/JF07-buffer-worker.sh
+++ scripts/JF07-buffer-worker.sh
@@ -14,9 +14,9 @@ M=$1
 #8 udp2buffer workers per core
 #coreAssociatedBuffer=(4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 6 6 6 6 6 6 6 6 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8)
 #4 udp2buffer workers per core
-#coreAssociatedBuffer=(4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8 9 9 9 9 10 10 10 10 11 11 11 11)
+coreAssociatedBuffer=(4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 8 8 8 8 9 9 9 9 10 10 10 10 11 11 11 11)
 #2 udp2buffer workers per core
-coreAssociatedBuffer=(4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16 17 17 18 18 19 19)
+#coreAssociatedBuffer=(4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16 17 17 18 18 19 19)
 # 1 udp2buffer worker per core
 #coreAssociatedBuffer=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
 
