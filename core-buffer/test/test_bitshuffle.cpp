6751a934e8f046e3d689b1aa2a7cc73e037f4160
Andrej Babic <andrej.babic@psi.ch>
2020-05-13 19:06:20 +0200

    Improve bitshuffle tests by using standard values

--- core-buffer/test/test_bitshuffle.cpp
+++ core-buffer/test/test_bitshuffle.cpp
@@ -6,12 +6,8 @@ using namespace std;
 
 TEST(bitshuffle, simple_compression)
 {
-    auto size = MODULE_N_PIXELS;
-    auto elem_size = 2; // uint16_t
-    auto block_size = MODULE_N_PIXELS;
-
     auto compress_buffer_size = bshuf_compress_lz4_bound(
-            size, elem_size, block_size);
+            MODULE_N_PIXELS, PIXEL_N_BYTES, MODULE_N_PIXELS);
 
     auto frame_buffer = make_unique<uint16_t[]>(MODULE_N_PIXELS);
     auto compress_buffer = make_unique<char[]>(compress_buffer_size);
@@ -22,14 +18,14 @@ TEST(bitshuffle, simple_compression)
 
     auto compressed_size = bshuf_compress_lz4(
             frame_buffer.get(), compress_buffer.get(),
-            size, elem_size, block_size);
+            MODULE_N_PIXELS, PIXEL_N_BYTES, MODULE_N_PIXELS);
 
     ASSERT_TRUE(compressed_size > 0);
 
     auto out_frame_buffer = make_unique<uint16_t[]>(MODULE_N_PIXELS);
     auto consumed_bytes = bshuf_decompress_lz4(
             compress_buffer.get(), out_frame_buffer.get(),
-            size, elem_size, block_size);
+            MODULE_N_PIXELS, PIXEL_N_BYTES, MODULE_N_PIXELS);
 
     ASSERT_TRUE(consumed_bytes > 0);
 
