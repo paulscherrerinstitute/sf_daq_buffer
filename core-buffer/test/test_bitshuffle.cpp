be0bb0226ee96ca4e34b426494d28dd585cf68c0
Andrej Babic <andrej.babic@psi.ch>
2020-05-13 14:08:11 +0200

    Check compressed data

--- core-buffer/test/test_bitshuffle.cpp
+++ core-buffer/test/test_bitshuffle.cpp
@@ -13,8 +13,12 @@ TEST(bitshuffle, simple_compression)
     auto compress_buffer_size = bshuf_compress_lz4_bound(
             size, elem_size, block_size);
 
-    auto frame_buffer = make_unique<char[]>(MODULE_N_BYTES);
+    auto frame_buffer = make_unique<uint16_t[]>(MODULE_N_PIXELS);
     auto compress_buffer = make_unique<char[]>(compress_buffer_size);
+
+    for (size_t i=0; i<MODULE_N_PIXELS; i++) {
+        frame_buffer[i] = i%100;
+    }
 
     auto compressed_size = bshuf_compress_lz4(
             frame_buffer.get(), compress_buffer.get(),
@@ -22,7 +26,7 @@ TEST(bitshuffle, simple_compression)
 
     ASSERT_TRUE(compressed_size > 0);
 
-    auto out_frame_buffer = make_unique<char[]>(MODULE_N_BYTES);
+    auto out_frame_buffer = make_unique<uint16_t[]>(MODULE_N_PIXELS);
     auto consumed_bytes = bshuf_decompress_lz4(
             compress_buffer.get(), out_frame_buffer.get(),
             size, elem_size, block_size);
@@ -30,4 +34,8 @@ TEST(bitshuffle, simple_compression)
     ASSERT_TRUE(consumed_bytes > 0);
 
     ASSERT_EQ(compressed_size, consumed_bytes);
+
+    for (size_t i=0; i<MODULE_N_PIXELS; i++) {
+        ASSERT_EQ(out_frame_buffer[i], frame_buffer[i]);
+    }
 }
