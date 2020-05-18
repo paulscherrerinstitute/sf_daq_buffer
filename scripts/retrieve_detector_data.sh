6fd1d433047515aaf6a99c45f51f8c2b5047f055
Andrej Babic <andrej.babic@psi.ch>
2020-05-18 18:04:41 +0200

    1 core per sf_replay

--- scripts/retrieve_detector_data.sh
+++ scripts/retrieve_detector_data.sh
@@ -16,7 +16,10 @@ STOP_PULSE_ID=$3
 #4 replay workers per core, last (writer) worker occupies 4
 #coreAssociated_replay=(20 20 20 20 21 21 21 21 22 22 22 22 23 23 23 23 24 24 24 24 25 25 25 25 26 26 26 26 27 27 27 27)
 #2 replay workers per core, last (writer) worker occupies 4
-coreAssociated_replay=(20 20 21 21 22 22 23 23 24 24 25 25 26 26 27 27 28 28 29 29 30 30 31 31 32 32 33 33 34 34 35 35)
+#coreAssociated_replay=(20 20 21 21 22 22 23 23 24 24 25 25 26 26 27 27 28 28 29 29 30 30 31 31 32 32 33 33 34 34 35 35)
+#1 replay workers per core, last (writer) worker occupies 4
+coreAssociated_replay=(8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
+
 
 coreAssociated_writer="24,25,26,27"
 
