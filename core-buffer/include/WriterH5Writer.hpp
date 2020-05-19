c17d02dea2bbf827833ff0b979b20e49880e6e09
Andrej Babic <andrej.babic@psi.ch>
2020-05-19 16:11:50 +0200

    Added new buffer object

--- core-buffer/include/WriterH5Writer.hpp
+++ core-buffer/include/WriterH5Writer.hpp
@@ -6,13 +6,23 @@
 #include <H5Cpp.h>
 #include "buffer_config.hpp"
 
-struct ImageMetadata
+struct ImageMetadataBuffer
 {
     uint64_t pulse_id[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
     uint64_t frame_index[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
     uint32_t daq_rec[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
     uint8_t is_good_frame[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
     uint64_t data_n_bytes[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
+    uint16_t n_pulses_in_buffer;
+};
+
+struct ImageMetadata
+{
+    uint64_t pulse_id;
+    uint64_t frame_index;
+    uint32_t daq_rec;
+    uint8_t is_good_frame;
+    uint64_t data_n_bytes;
 };
 
 class WriterH5Writer {
