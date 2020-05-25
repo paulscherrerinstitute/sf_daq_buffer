183f9e3eaaac480cca0273650fe0eec7f2cd9242
Andrej Babic <andrej.babic@psi.ch>
2020-05-25 14:25:55 +0200

    Fix ImageBuffer size

--- core-buffer/include/formats.hpp
+++ core-buffer/include/formats.hpp
@@ -6,21 +6,11 @@
 
 struct ImageMetadataBuffer
 {
-    uint64_t pulse_id[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint64_t frame_index[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint32_t daq_rec[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint8_t is_good_frame[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint64_t data_n_bytes[core_buffer::REPLAY_READ_BUFFER_SIZE];
+    uint64_t pulse_id[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
+    uint64_t frame_index[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
+    uint32_t daq_rec[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
+    uint8_t is_good_frame[core_buffer::WRITER_DATA_CACHE_N_IMAGES];
     uint16_t n_images;
-};
-
-struct ImageMetadata
-{
-    uint64_t pulse_id;
-    uint64_t frame_index;
-    uint32_t daq_rec;
-    uint8_t is_good_frame;
-    uint64_t data_n_bytes;
 };
 
 #pragma pack(push)
