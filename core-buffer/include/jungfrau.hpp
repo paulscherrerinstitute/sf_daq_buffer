1e7af3df326b287ddc2f0d4e37e31d96b1b60c3e
Andrej Babic <andrej.babic@psi.ch>
2020-05-25 11:02:55 +0200

    Optimize ReplayModuleFrameBuffer

--- core-buffer/include/jungfrau.hpp
+++ core-buffer/include/jungfrau.hpp
@@ -54,16 +54,12 @@ struct ModuleFrame {
 #pragma pack(push)
 #pragma pack(1)
 struct ReplayModuleFrameBuffer {
-    uint64_t pulse_id[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint64_t frame_index[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint64_t daq_rec[core_buffer::REPLAY_READ_BUFFER_SIZE];
-    uint64_t n_received_packets[core_buffer::REPLAY_READ_BUFFER_SIZE];
+    ModuleFrame frame[core_buffer::REPLAY_READ_BUFFER_SIZE];
     bool is_frame_present[core_buffer::REPLAY_READ_BUFFER_SIZE];
     bool is_good_frame[core_buffer::REPLAY_READ_BUFFER_SIZE];
     uint16_t module_id;
     uint64_t data_n_bytes;
     uint16_t n_frames;
-
 };
 #pragma pack(pop)
 
