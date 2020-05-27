7d619aa90e3ccd48ff0da16f740fed35bd09cb5f
Andrej Babic <andrej.babic@psi.ch>
2020-05-27 14:24:21 +0200

    Add buffer retrieval methods

--- sf-writer/src/ImageAssembler.cpp
+++ sf-writer/src/ImageAssembler.cpp
@@ -16,10 +16,20 @@ ImageAssembler::~ImageAssembler()
     delete[] metadata_buffer_;
 }
 
+int ImageAssembler::get_free_slot()
+{
+
+}
+
 void ImageAssembler::process(
         const int slot_id,
         const int i_module,
         const BufferBinaryBlock* block_buffer)
+{
+
+}
+
+int ImageAssembler::get_full_slot()
 {
 
 }
@@ -29,12 +39,12 @@ void ImageAssembler::free_slot(const int slot_id)
 
 }
 
-char* ImageAssembler::get_data_buffer(const int slot_id)
-{
-
-}
-
 ImageMetadataBlock* ImageAssembler::get_metadata_buffer(const int slot_id)
 {
+    return &(metadata_buffer_[slot_id]);
+}
 
+char* ImageAssembler::get_data_buffer(const int slot_id)
+{
+    return image_buffer_ + (slot_id * MODULE_N_BYTES * n_modules_);
 }
