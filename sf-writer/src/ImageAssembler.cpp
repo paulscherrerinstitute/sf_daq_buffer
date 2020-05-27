75c17247407288d39b92abb5709fef9ed97b3698
Andrej Babic <andrej.babic@psi.ch>
2020-05-27 14:11:21 +0200

    Add empty stub implementation of ImageAssembler

--- sf-writer/src/ImageAssembler.cpp
+++ sf-writer/src/ImageAssembler.cpp
@@ -1,1 +1,44 @@
 #include "ImageAssembler.hpp"
+
+ImageAssembler::ImageAssembler()
+{
+
+}
+
+ImageAssembler::~ImageAssembler()
+{
+
+}
+
+void ImageAssembler::process(
+        const int slot_id,
+        const int i_module,
+        const BufferBinaryBlock* block_buffer)
+{
+
+}
+
+void ImageAssembler::get_free_slot()
+{
+
+}
+
+void ImageAssembler::get_full_slot()
+{
+
+}
+
+void ImageAssembler::free_slot(const int slot_id)
+{
+
+}
+
+char* ImageAssembler::get_data_buffer(const int slot_id)
+{
+
+}
+
+ImageMetadataBlock* ImageAssembler::get_metadata_buffer(const int slot_id)
+{
+
+}
