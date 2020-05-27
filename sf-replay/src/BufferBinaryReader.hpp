2003d9c982e721b3884b926b5fc2107cf0202ed3
Andrej Babic <andrej.babic@psi.ch>
2020-05-27 11:13:55 +0200

    Definition of BufferBinaryReader

---
+++ sf-replay/src/BufferBinaryReader.hpp
@@ -0,0 +1,28 @@
+#ifndef SF_DAQ_BUFFER_BUFFERBINARYREADER_HPP
+#define SF_DAQ_BUFFER_BUFFERBINARYREADER_HPP
+
+
+#include <formats.hpp>
+
+class BufferBinaryReader {
+
+    const std::string device_;
+    const std::string channel_name_;
+
+    std::string current_input_file_;
+    int input_file_fd_;
+
+    void open_file(const std::string& filename);
+    void close_current_file();
+
+public:
+    BufferBinaryReader(const std::string &device,
+                       const std::string &channel_name);
+
+    virtual ~BufferBinaryReader();
+
+    void get_block(const uint64_t block_number, BufferBinaryBlock *buffer);
+};
+
+
+#endif //SF_DAQ_BUFFER_BUFFERBINARYREADER_HPP
