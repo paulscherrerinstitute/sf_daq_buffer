ba383f11f6d574c98505c66294e53b1378402cfa
Andrej Babic <andrej.babic@psi.ch>
2020-05-27 13:07:16 +0200

    Improve ImageAssembler interface

--- sf-writer/src/ImageAssembler.hpp
+++ sf-writer/src/ImageAssembler.hpp
@@ -3,6 +3,16 @@
 
 
 class ImageAssembler {
+public:
+    void process(const int slot_id,
+                 const int i_module,
+                 const BufferBinaryBlock* block_buffer);
+    int get_free_slot();
+    int get_full_slot();
+    void free_slot(int slot_id);
+
+    const ImageMetadataBlock* get_metadata_buffer(int slot_id);
+    const char* get_data_buffer(int slot_id);
 
 };
 
