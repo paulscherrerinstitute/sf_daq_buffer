cca0572d3ee8fd97dc12cdf359e31b5d6980bbc2
Andrej Babic <andrej.babic@psi.ch>
2020-06-09 10:58:11 +0200

    Adapt live RecvModule

--- sf-stream/include/LiveRecvModule.hpp
+++ sf-stream/include/LiveRecvModule.hpp
@@ -7,30 +7,25 @@
 #include "FastQueue.hpp"
 #include "jungfrau.hpp"
 #include "formats.hpp"
+#include "ZmqLiveReceiver.hpp"
+
 
 class LiveRecvModule {
 
     FastQueue<ModuleFrameBuffer>& queue_;
-    const size_t n_modules_;
-    void* ctx_;
-    const std::string ipc_prefix_;
+    ZmqLiveReceiver& receiver_;
+
     std::atomic_bool is_receiving_;
     std::thread receiving_thread_;
 
-    void* connect_socket(size_t module_id);
     void receive_thread();
-    void recv_single_module(void* socket, ModuleFrame* meta, char* data);
-    uint64_t align_modules(const std::vector<void*>& sockets,
-                           ModuleFrameBuffer *meta,
-                           char *data);
+
     void stop();
 
 public:
     LiveRecvModule(
             FastQueue<ModuleFrameBuffer>& queue,
-            const size_t n_modules,
-            void* ctx,
-            const std::string& ipc_prefix);
+            ZmqLiveReceiver& receiver);
 
     ~LiveRecvModule();
 };
