c6960b5e59c3c3d941f4a63fe61ec8ff5d3c3601
Andrej Babic <andrej.babic@psi.ch>
2020-06-09 10:58:47 +0200

    Extract Receiver from module for Zmq recv and assembly

---
+++ sf-stream/include/ZmqLiveReceiver.hpp
@@ -0,0 +1,33 @@
+#ifndef SF_DAQ_BUFFER_ZMQLIVERECEIVER_HPP
+#define SF_DAQ_BUFFER_ZMQLIVERECEIVER_HPP
+
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "formats.hpp"
+
+class ZmqLiveReceiver {
+
+    const size_t n_modules_;
+    void* ctx_;
+    const std::string ipc_prefix_;
+    std::vector<void*> sockets_;
+
+    void* connect_socket(size_t module_id);
+    void recv_single_module(void* socket, ModuleFrame* meta, char* data);
+    uint64_t align_modules(ModuleFrameBuffer *meta, char *data);
+
+public:
+    ZmqLiveReceiver(const size_t n_modules,
+                    void* ctx,
+                    const std::string& ipc_prefix);
+
+    ~ZmqLiveReceiver();
+
+    void get_next_image(ModuleFrameBuffer* meta, char* data);
+};
+
+
+#endif //SF_DAQ_BUFFER_ZMQLIVERECEIVER_HPP
