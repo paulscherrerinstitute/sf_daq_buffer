402a2b978d002bbfa41d7e435e23f7ce77f9f90e
Andrej Babic <andrej.babic@psi.ch>
2021-01-19 16:58:07 +0100

    Rework sf-stream to attach to assembler

--- sf-stream/src/main.cpp
+++ sf-stream/src/main.cpp
@@ -5,10 +5,8 @@
 #include <BufferUtils.hpp>
 #include <StreamStats.hpp>
 
-#include "buffer_config.hpp"
 #include "stream_config.hpp"
 #include "ZmqLiveSender.hpp"
-#include "ZmqPulseSyncReceiver.hpp"
 
 using namespace std;
 using namespace buffer_config;
@@ -30,22 +28,22 @@ int main (int argc, char *argv[])
     // TODO: Add stream_name to config reading - multiple stream definitions.
     auto config = BufferUtils::read_json_config(string(argv[1]));
 
-    string RECV_IPC_URL = BUFFER_LIVE_IPC_URL + config.detector_name + "-";
     auto ctx = zmq_ctx_new();
     zmq_ctx_set(ctx, ZMQ_IO_THREADS, STREAM_ZMQ_IO_THREADS);
+    auto receiver = BufferUtils::connect_socket(
+            ctx, config.detector_name, "assembler");
 
-    ZmqPulseSyncReceiver receiver(ctx, config.detector_name, config.n_modules);
     RamBuffer ram_buffer(config.detector_name, config.n_modules);
     StreamStats stats(config.detector_name, stream_name, STREAM_STATS_MODULO);
     ZmqLiveSender sender(ctx, config);
 
     ImageMetadata meta;
     while (true) {
-        auto pulse_and_sync = receiver.get_next_pulse_id();
-        char* data = ram_buffer.read_image(pulse_and_sync.pulse_id, meta);
+        zmq_recv(receiver, &meta, sizeof(meta), 0);
+        char* data = ram_buffer.read_image(meta.pulse_id);
 
         sender.send(meta, data);
 
-        stats.record_stats(meta, pulse_and_sync.n_lost_pulses);
+        stats.record_stats(meta);
     }
 }
