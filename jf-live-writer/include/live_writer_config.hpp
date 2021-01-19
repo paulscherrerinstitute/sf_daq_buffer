6bbbd734c7f0ec9901a4e4dc3929d477bf635ceb
Andrej Babic <andrej.babic@psi.ch>
2021-01-19 17:20:06 +0100

    Add jf-live-writer
    First implementation of the image buffer writer for the
     Jungfrau

--- jf-live-writer/include/live_writer_config.hpp
+++ jf-live-writer/include/live_writer_config.hpp
@@ -2,8 +2,6 @@
 
 namespace live_writer_config
 {
-    // MS to retry reading from the image assembler.
-    const size_t ASSEMBLER_RETRY_MS = 5;
-    // Number of slots in the reconstruction buffer.
-    const size_t WRITER_IA_N_SLOTS = 200;
+    // N of IO threads to receive data from modules.
+    const int LIVE_ZMQ_IO_THREADS = 1;
 }
