2a9e4f2b7c8b450f8a888dbe45864d4f09c60eb3
Andrej Babic <andrej.babic@psi.ch>
2021-01-19 18:35:28 +0100

    Implementation of WriterStats for the live writer

--- jf-live-writer/include/WriterStats.hpp
+++ jf-live-writer/include/WriterStats.hpp
@@ -6,12 +6,11 @@
 #define SF_DAQ_BUFFER_FRAMESTATS_HPP
 
 
-class BufferStats {
+class WriterStats {
     const std::string detector_name_;
-    const int module_id_;
     size_t stats_modulo_;
 
-    int frames_counter_;
+    int image_counter_;
     uint32_t total_buffer_write_us_;
     uint32_t max_buffer_write_us_;
     std::chrono::time_point<std::chrono::steady_clock> stats_interval_start_;
@@ -20,12 +19,11 @@ class BufferStats {
     void print_stats();
 
 public:
-    BufferStats(
+    WriterStats(
             const std::string &detector_name,
-            const int module_id,
             const size_t stats_modulo);
-    void start_frame_write();
-    void end_frame_write();
+    void start_image_write();
+    void end_image_write();
 };
 
 
