d0df2677defea40fdbd37cfbc5875db82e7852b3
Andrej Babic <andrej.babic@psi.ch>
2021-02-17 12:46:58 +0100

    Remove old writer

--- sf-writer/include/writer_config.hpp
+++
@@ -1,9 +0,0 @@
-#include <cstddef>
-
-namespace writer_config
-{
-    // MS to retry reading from the image assembler.
-    const size_t ASSEMBLER_RETRY_MS = 5;
-    // Number of slots in the reconstruction buffer.
-    const size_t WRITER_IA_N_SLOTS = 2;
-}
