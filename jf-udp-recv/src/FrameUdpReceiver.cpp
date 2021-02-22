0ede084a7b3954211b40a9cb963ee59f42c5e4da
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2021-02-22 18:06:18 +0100

    less debug msgs

--- jf-udp-recv/src/FrameUdpReceiver.cpp
+++ jf-udp-recv/src/FrameUdpReceiver.cpp
@@ -47,13 +47,13 @@ inline void FrameUdpReceiver::init_frame(
     frame_metadata.frame_index = packet_buffer_[i_packet].framenum;
     frame_metadata.daq_rec = (uint64_t) packet_buffer_[i_packet].debug;
     frame_metadata.module_id = (int64_t) module_id_;
-    #ifdef DEBUG_OUTPUT
-        using namespace date;
-        cout << " [" << std::chrono::system_clock::now();
-        cout << "] [FrameUdpReceiver::init_frame] :";
-        cout << " Frame number: " << frame_metadata.frame_index << endl;
-        cout << endl;
-    #endif
+    // #ifdef DEBUG_OUTPUT
+    //     using namespace date;
+    //     cout << " [" << std::chrono::system_clock::now();
+    //     cout << "] [FrameUdpReceiver::init_frame] :";
+    //     cout << " Frame number: " << frame_metadata.frame_index << endl;
+    //     cout << endl;
+    // #endif
 }
 
 inline void FrameUdpReceiver::copy_packet_to_buffers(
