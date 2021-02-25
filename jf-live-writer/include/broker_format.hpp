592741048a6dd50b69f827707589a9ff38c8b941
Andrej Babic <andrej.babic@psi.ch>
2021-02-25 10:47:46 +0100

    Add image details to store stream format

--- jf-live-writer/include/broker_format.hpp
+++ jf-live-writer/include/broker_format.hpp
@@ -9,10 +9,13 @@ const static uint8_t OP_END = 2;
 #pragma pack(push)
 #pragma pack(1)
 struct StoreStream {
-    uint8_t op_code;
+    int64_t run_id;
     uint32_t i_image;
     uint32_t n_images;
-    int64_t run_id;
+    uint32_t image_y_size;
+    uint32_t image_x_size;
+    uint32_t op_code;
+    uint32_t bits_per_pixel;
 
     ImageMetadata image_metadata;
 };
