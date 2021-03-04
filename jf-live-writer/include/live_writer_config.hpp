ea81c74a3bcd21abd27be9ba7cd306ccd746fecc
Andrej Babic <andrej.babic@psi.ch>
2021-03-04 15:56:30 +0100

    Small bug fix in live writer config

--- jf-live-writer/include/live_writer_config.hpp
+++ jf-live-writer/include/live_writer_config.hpp
@@ -5,5 +5,5 @@ namespace live_writer_config
     // N of IO threads to receive data from modules.
     const int LIVE_ZMQ_IO_THREADS = 1;
 
-    const std::string OUTPUT_FOLDER_SYMLINK = "OUTPUT/"
+    const std::string OUTPUT_FOLDER_SYMLINK = "OUTPUT/";
 }
