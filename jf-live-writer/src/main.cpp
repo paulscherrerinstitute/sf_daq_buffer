d339d06c6f8f19dfbb86b9b19caf3d00b72b57ea
Andrej Babic <andrej.babic@psi.ch>
2021-04-14 17:19:23 +0200

    Renamed broker agent to writer agent for the writer driver

--- jf-live-writer/src/main.cpp
+++ jf-live-writer/src/main.cpp
@@ -38,7 +38,7 @@ int main (int argc, char *argv[])
     auto ctx = zmq_ctx_new();
     zmq_ctx_set(ctx, ZMQ_IO_THREADS, LIVE_ZMQ_IO_THREADS);
     auto receiver = BufferUtils::connect_socket(
-            ctx, config.detector_name, "broker-agent");
+            ctx, config.detector_name, "writer-agent");
 
     RamBuffer ram_buffer(config.detector_name, config.n_modules);
 
