7879c3c3ea8402580b6f1c3e122f7bca3d3b08e1
Andrej Babic <andrej.babic@psi.ch>
2021-07-02 14:41:48 +0200

    Move JF specific receiver to generic

--- std-udp-recv/src/main.cpp
+++ std-udp-recv/src/main.cpp
@@ -20,27 +20,24 @@ int main (int argc, char *argv[]) {
 
     if (argc != 4) {
         cout << endl;
-        #ifndef USE_EIGER
-            cout << "Usage: jf_udp_recv [detector_json_filename] [module_id] [bit_depth]";
-        #else
-            cout << "Usage: eiger_udp_recv [detector_json_filename] [module_id] [bit_depth]";
-        #endif
+
+        cout << "Usage: std_udp_recv [udp_recv_config_filename] [module_id] [bit_depth]";
         cout << endl;
-        cout << "\tdetector_json_filename: detector config file path." << endl;
+        cout << "\tudp_recv_config_filename: detector config file path." << endl;
         cout << "\tmodule_id: id of the module for this process." << endl;
         cout << "\tbit_depth: bit depth of the incoming udp packets." << endl;
         cout << endl;
         exit(-1);
     }
 
-    const auto config = read_json_config(string(argv[1]));
+    const auto config = UdpRecvConfig.from_json_file(string(argv[1]));
     const int module_id = atoi(argv[2]);
     const int bit_depth = atoi(argv[3]);
     const int n_receivers = config.n_modules * config.n_submodules;
     const auto udp_port = config.start_udp_port + module_id;
     
     FrameUdpReceiver receiver(module_id, udp_port, n_receivers, config.n_submodules, bit_depth);
-    RamBuffer buffer(config.detector_name, n_receivers, config.n_submodules, bit_depth);
+    RamBuffer frame_buffer(config.detector_name, n_receivers, config.n_submodules, bit_depth);
     FrameStats stats(config.detector_name, n_receivers, module_id, bit_depth, STATS_TIME);
 
     auto ctx = zmq_ctx_new();
@@ -62,7 +59,7 @@ int main (int argc, char *argv[]) {
              ( (meta.frame_index-frame_index_previous) > 1000 ) ){  
             bad_pulse_id = true;
         } else { 
-            buffer.write_frame(meta, data);
+            frame_buffer.write_frame(meta, data);
 
             zmq_send(socket, &pulse_id, sizeof(pulse_id), 0);
 
