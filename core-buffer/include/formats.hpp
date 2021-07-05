7b4b750914dca8e29944cef68c4499767ad4491d
Andrej Babic <andrej.babic@psi.ch>
2021-07-05 14:45:09 +0200

    Adjust formats to latest standard

--- core-buffer/include/formats.hpp
+++ core-buffer/include/formats.hpp
@@ -3,25 +3,19 @@
 
 #include "buffer_config.hpp"
 
-#ifndef USE_EIGER
-#include "jungfrau.hpp"
-#else
-#include "eiger.hpp"
-#endif
-
-#define IS_BOTTOM(n) ((n%2 != 0) ? -1 : 1)
 
 #pragma pack(push)
 #pragma pack(1)
 struct ModuleFrame {
+    uint64_t id;
     uint64_t pulse_id;
     uint64_t frame_index;
     uint64_t daq_rec;
     uint64_t n_recv_packets;
     uint64_t module_id;
     uint16_t bit_depth;
-    uint16_t row;
-    uint16_t column;
+    uint16_t pos_y;
+    uint16_t pos_x;
 };
 #pragma pack(pop)
 
@@ -36,26 +30,5 @@ struct ImageMetadata {
 };
 #pragma pack(pop)
 
-//struct ModuleFrameBuffer {
-//    ModuleFrame module[N_MODULES];
-//};
-
-//#pragma pack(push)
-//#pragma pack(1)
-//struct BufferBinaryFormat {
-//    const char FORMAT_MARKER = 0xBE;
-//    ModuleFrame meta;
-//    char data[MODULE_N_BYTES];
-//};
-//#pragma pack(pop)
-
-//#pragma pack(push)
-//#pragma pack(1)
-//struct BufferBinaryBlock
-//{
-//    BufferBinaryFormat frame[buffer_config::BUFFER_BLOCK_SIZE];
-//    uint64_t start_pulse_id;
-//};
-//#pragma pack(pop)
 
 #endif //SF_DAQ_BUFFER_FORMATS_HPP
