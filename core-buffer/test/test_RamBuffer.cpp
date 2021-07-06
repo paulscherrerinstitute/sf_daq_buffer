45ca6dcd13e519a94c1fdabf85fc85175dd78bca
Andrej Babic <andrej.babic@psi.ch>
2021-07-06 12:31:51 +0200

    Fix core buffer tests

--- core-buffer/test/test_RamBuffer.cpp
+++ core-buffer/test/test_RamBuffer.cpp
@@ -4,16 +4,21 @@
 using namespace std;
 using namespace buffer_config;
 
+
 TEST(RamBuffer, simple_store)
 {
     const int n_modules = 3;
-    RamBuffer buffer("test_detector", n_modules, 10);
+    const size_t DATA_N_BYTES = MODULE_N_PIXELS * 2;
+
+    RamBuffer buffer("test_detector",
+            sizeof(ModuleFrame), DATA_N_BYTES, n_modules, 10);
 
     ModuleFrame frame_meta;
+    frame_meta.id = 12345678;
     frame_meta.pulse_id = 123523;
     frame_meta.daq_rec = 1234;
     frame_meta.frame_index = 12342300;
-    frame_meta.n_recv_packets = EIGER_N_PACKETS_PER_FRAME;
+    frame_meta.n_recv_packets = 128;
 
     auto frame_buffer = make_unique<uint16_t[]>(MODULE_N_PIXELS);
 
@@ -28,10 +33,22 @@ TEST(RamBuffer, simple_store)
         buffer.write_frame(frame_meta, (char *) (frame_buffer.get()));
     }
 
-    ImageMetadata image_meta;
-    buffer.assemble_image(frame_meta.pulse_id, image_meta);
-    ASSERT_EQ(image_meta.pulse_id, frame_meta.pulse_id);
-    ASSERT_EQ(image_meta.daq_rec, frame_meta.daq_rec);
-    ASSERT_EQ(image_meta.frame_index, frame_meta.frame_index);
-    ASSERT_EQ(image_meta.is_good_image, 1);
+    auto meta_buffer = (ModuleFrame*) buffer.get_slot_meta(frame_meta.id);
+    for (int i_module=0; i_module<n_modules; i_module++) {
+        ASSERT_EQ(meta_buffer[i_module].id, frame_meta.id);
+        ASSERT_EQ(meta_buffer[i_module].pulse_id, frame_meta.pulse_id);
+        ASSERT_EQ(meta_buffer[i_module].daq_rec, frame_meta.daq_rec);
+        ASSERT_EQ(meta_buffer[i_module].frame_index, frame_meta.frame_index);
+        ASSERT_EQ(meta_buffer[i_module].module_id, i_module);
+    }
+
+    auto data_buffer = (uint16_t*) buffer.get_slot_data(frame_meta.id);
+    for (int i_module=0; i_module<n_modules; i_module++) {
+        auto module_buffer = data_buffer + (MODULE_N_PIXELS * i_module);
+
+        for (size_t i = 0; i < MODULE_N_PIXELS; i++) {
+            ASSERT_EQ(module_buffer[i], i%100);
+        }
+    }
+
 }
