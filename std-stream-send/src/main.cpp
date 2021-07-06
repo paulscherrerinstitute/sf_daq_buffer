881491220ebf1de4ade6fe9f89c84454cb103ed7
Andrej Babic <andrej.babic@psi.ch>
2021-07-06 11:24:11 +0200

    Add config to streamer

--- std-stream-send/src/main.cpp
+++ std-stream-send/src/main.cpp
@@ -3,14 +3,30 @@
 #include "stream_config.hpp"
 #include <chrono>
 #include <thread>
+#include <StreamSendConfig.hpp>
 #include "RamBuffer.hpp"
 
 
 using namespace std;
 using namespace stream_config;
+using namespace buffer_config;
 
 int main (int argc, char *argv[])
 {
+    if (argc != 3) {
+        cout << endl;
+        cout << "Usage: std_stream_send [detector_json_filename]"
+                " [bit_depth]" << endl;
+        cout << "\tdetector_json_filename: detector config file path." << endl;
+        cout << "\tbit_depth: bit depth of the incoming udp packets." << endl;
+        cout << endl;
+
+        exit(-1);
+    }
+
+    const auto config = StreamSendConfig::from_json_file(string(argv[1]));
+    const int bit_depth = atoi(argv[2]);
+
     auto ctx = zmq_ctx_new();
     zmq_ctx_set(ctx, ZMQ_IO_THREADS, STREAM_ZMQ_IO_THREADS);
 
@@ -27,12 +43,14 @@ int main (int argc, char *argv[])
         throw runtime_error(zmq_strerror(errno));
     }
 
-    if (zmq_bind(sender, "tcp://127.0.0.1:10000") != 0) {
+    if (zmq_bind(sender, config.stream_address.c_str()) != 0) {
         throw runtime_error(zmq_strerror(errno));
     }
 
+    const size_t IMAGE_N_BYTES = config.image_n_pixels * bit_depth / 8;
     RamBuffer image_buffer(config.detector_name + "_assembler",
-            sizeof(ImageMetadata), assembler.get_image_n_bytes(), 1);
+            sizeof(ImageMetadata), IMAGE_N_BYTES,
+            config.n_modules, RAM_BUFFER_N_SLOTS);
 
     while (true) {
 
