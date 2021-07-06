2fbead8c2ff44786b8ef86d94602801477c50f41
Andrej Babic <andrej.babic@psi.ch>
2021-07-06 16:21:11 +0200

    Remove unused values from StoreStream

--- std-det-writer/include/broker_format.hpp
+++ std-det-writer/include/broker_format.hpp
@@ -7,14 +7,11 @@
 #pragma pack(push)
 #pragma pack(1)
 struct StoreStream {
-    ImageMetadata image_metadata;
-
+    std::string output_file;
     int64_t run_id;
+    uint64_t image_id;
     uint32_t i_image;
     uint32_t n_images;
-    uint32_t image_y_size;
-    uint32_t image_x_size;
-    uint32_t bits_per_pixel;
 };
 #pragma pack(pop)
 #endif //SF_DAQ_BUFFER_BROKER_FORMAT_HPP
