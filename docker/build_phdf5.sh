ea29dd5655735a08d151893154e55f7f4c9f9afa
Andrej Babic <andrej.babic@psi.ch>
2021-07-12 17:57:15 +0200

    Add anaconda to the base container for the daq debug

--- docker/build_phdf5.sh
+++ docker/build_phdf5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.0.0
+VERSION=1.0.1
 
 docker build --no-cache=true -f phdf5.Dockerfile -t paulscherrerinstitute/sf-daq_phdf5 .
 docker tag paulscherrerinstitute/sf-daq_phdf5 paulscherrerinstitute/sf-daq_phdf5:$VERSION
