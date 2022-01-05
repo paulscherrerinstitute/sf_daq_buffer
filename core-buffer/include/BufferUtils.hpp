48b68668f4dcb3481925e74efd05f4e91c47d369
babic_a <andrej.babic@psi.ch>
2022-01-05 23:45:45 +0100

    bugfix order of params from json config

--- core-buffer/include/BufferUtils.hpp
+++ core-buffer/include/BufferUtils.hpp
@@ -12,10 +12,10 @@ namespace BufferUtils
         const std::string detector_name;
         const std::string detector_type;
         const int n_modules;
+        const int bit_depth;
         const int image_height;
         const int image_width;
         const int start_udp_port;
-        const int bit_depth;
         
 
         friend std::ostream& operator <<(std::ostream& os, DetectorConfig const& det_config)
@@ -23,10 +23,10 @@ namespace BufferUtils
                 return os << det_config.detector_name << ' '
                         << det_config.detector_type << ' '
                         << det_config.n_modules << ' '
-                        << det_config.start_udp_port << ' '
                         << det_config.bit_depth << ' '
                         << det_config.image_height << ' '
-                        << det_config.image_width << ' ';
+                        << det_config.image_width << ' '
+                        << det_config.start_udp_port << ' ';
         }
     };
 
--- std-udp-recv/include/UdpRecvConfig.hpp
+++ std-udp-recv/include/UdpRecvConfig.hpp
@@ -20,6 +20,8 @@ struct UdpRecvConfig {
                 config_parameters["detector_type"].GetString(),
                 config_parameters["n_modules"].GetInt(),
                 config_parameters["bit_depth"].GetInt(),
+                config_parameters["image_height"].GetInt(),
+                config_parameters["image_width"].GetInt(),
                 config_parameters["start_udp_port"].GetInt(),
         };
     }
@@ -27,8 +29,10 @@ struct UdpRecvConfig {
     const std::string detector_name;
     const std::string detector_type;
     const int n_modules;
-    const int start_udp_port;
     const int bit_depth;
+    const int image_height;
+    const int image_width;
+    const int start_udp_port;
 };
 
 
